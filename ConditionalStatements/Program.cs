5cea5016a68c6c161d0e3c1cfeb83f80477a0dce
HFGF <HFGF@DESKTOP-0H5SSK4>
2024-08-06 13:24:13 +0200

    Add project files.

---
+++ ConditionalStatements/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(AbsoluteValue(6832));
+            //Console.WriteLine(AbsoluteValue(-392));
+
+            //Console.WriteLine(DivisibleBy2Or3(15,30));
+            //Console.WriteLine(DivisibleBy2Or3(2,90));
+            //Console.WriteLine(DivisibleBy2Or3(7,12));
+
+            //Console.WriteLine(IfConsistsOfUppercaseLetters("xyz"));
+            //Console.WriteLine(IfConsistsOfUppercaseLetters("DOG"));
+            //Console.WriteLine(IfConsistsOfUppercaseLetters("L9#"));
+
+            //Console.WriteLine(IfGreaterThanThirdOne([2, 7, 12]));
+            //Console.WriteLine(IfGreaterThanThirdOne([-5, -8, 50]));
+
+            //Console.WriteLine(IfNumberIsEven(721));
+            //Console.WriteLine(IfNumberIsEven(1248));
+
+            //Console.WriteLine(IfSortedArray([3, 7, 10]));
+            //Console.WriteLine(IfSortedArray([74,62,99]));
+
+            Console.WriteLine(PositiveNegativeOrZero(5.24));
+            Console.WriteLine(PositiveNegativeOrZero(0.0));
+            Console.WriteLine(PositiveNegativeOrZero(-994.53));
+
+
+        }
+
+        static int AbsoluteValue(int val)
+        {
+            int output = Math.Abs(val);
+            return output;
+        }
+
+        static int DivisibleBy2Or3(int a, int b)
+        {
+            int output;
+            if ((a % 2 == 0 || a % 3 == 0) && (b % 2 == 0 || b % 3 == 0))
+            {
+                output = a * b;
+
+            }
+            else
+            {
+                output = a + b;
+            }
+            return output;
+        }
+
+        static bool IfConsistsOfUppercaseLetters(string a)
+        {
+            return a.All(Char.IsUpper);
+        }
+
+        static bool IfGreaterThanThirdOne(int[] a)
+        {
+            if (a[0] + a[1] > a[2] == true || a[0] * a[1] > a[2] == true)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+
+        static bool IfNumberIsEven(int a)
+        {
+            if (a % 2 == 0)
+            {
+            return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IfSortedArray(int[] a)
+        {
+            int temp = a[0];
+            foreach (int i in a)
+            {
+                if (i < temp == true)
+                {
+                    return false;
+                }
+                temp = i;
+                
+            }
+            return true;
+        }
+
+        static string PositiveNegativeOrZero(double a)
+        {
+            if (a > 0)
+            {
+                return "positive";
+            }
+            else if (a < 0)
+            {
+                return "negative";
+            }
+            else
+            {
+                return "zero";
+            }
+        }
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(AddAndMultiply(2, 4, 5));
+
+            //Console.WriteLine(CtoF(0));
+            //Console.WriteLine(CtoF(100));
+            //Console.WriteLine(CtoF(-300));
+
+            //Console.WriteLine(ElementaryOperations(3,8));
+            //Console.WriteLine(IsResultTheSame(2 + 2, 2 * 2));
+            //Console.WriteLine(IsResultTheSame(9/3, 16-1));
+
+            //Console.WriteLine(ModuloOperations(8, 5, 2));
+
+            //Console.WriteLine(CubeOf(2));
+            //Console.WriteLine(CubeOf(-5.5));
+
+            SwapTwoNumbers(87, 45);
+            SwapTwoNumbers(-13, 2);
+        }
+
+        static int AddAndMultiply(int a, int b, int c)
+        {
+            int output = (a + b) * c;
+            
+            return output;
+        }
+
+        static string CtoF(double a) 
+        {
+            string output = "T = " + (a * 9 / 5 + 32).ToString() + "F";
+            if (a < -271.15)
+            {
+                string failMessage = "Temperature below absolute zero!";
+                return failMessage;
+            }
+            else
+            {
+                return output;
+            }
+            
+        }
+
+        static string ElementaryOperations(double a, double b)
+        {
+            string output = $"{a + b}, {a - b}, {a * b}, {a / b} ";
+            if (b ==0)
+            {
+                output = "Remember that you can't divide any number by 0!";
+            }
+            return output;
+        }
+
+        static bool IsResultTheSame(int a, int b)
+        {
+            return a == b;
+        }
+
+        static int ModuloOperations(int a, int b, int c)
+        {
+            int output = a % b % c;
+            return output;
+        }
+
+        static double CubeOf(double a)
+        {
+            double output = Math.Pow(a, 3);
+            return output;
+        }
+
+        static void SwapTwoNumbers(int a, int b)
+        {
+            Console.WriteLine($"Before: a = {a}, b = {b};");
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine($"After: a = {a}, b = {b};");
+        }
+
+
+    }
+}
