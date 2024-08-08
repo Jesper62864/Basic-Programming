281aadcb41677bf7fabbacab936ff3344675cc02
Jesper Weble <62864@edu.sde.dk>
2024-08-08 11:17:50 +0200

    Finished all remaining exercises for Loops.

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Loops
+﻿
+using System;
+using System.Diagnostics.Metrics;
+
+namespace Loops
 {
     internal class Program
     {
@@ -15,8 +19,23 @@
             //ThreeIncreasingAdjacent([45, 23, 44, 68, 65, 70, 80, 81, 82]);
             //ThreeIncreasingAdjacent([7, 3, 5, 8, 9, 3, 1, 4]);
 
-            SieveOfEratosthenes(30);
+            //SieveOfEratosthenes(30);
 
+            //ExtractString("##abc##def");
+            //ExtractString("12####78");
+            //ExtractString("gar##d#en");
+            //ExtractString("++##--##++");
+
+            //FullSequenceOfLetters("ds");
+            //FullSequenceOfLetters("or");
+
+            //SumAndAverage(11, 66);
+            //SumAndAverage(-10, 0);
+
+            //DrawTriangle();
+
+            ToThePowerOf(-2, 3);
+            ToThePowerOf(5, 5);
         }
 
         static void MultiplicationTable()
@@ -102,5 +121,83 @@
             }
             
         }
+
+        static void ExtractString(string v)
+        {
+            int firstOccurance = v.IndexOf("#");
+            int lastOccurance  = v.LastIndexOf("#");
+            string output = v.Substring(firstOccurance +2, lastOccurance - firstOccurance -3);
+            if (output == "")
+            {
+                Console.WriteLine("empty string");
+            }
+            else
+            {
+                Console.WriteLine(output);                
+            }
+
+                
+        
+        }
+
+        static void FullSequenceOfLetters(string v)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            int startIndex = alphabet.IndexOf(v[0]);
+            int endIndex = alphabet.IndexOf(v[1]);
+            string output = alphabet.Substring(startIndex, endIndex - startIndex +1);
+            Console.WriteLine(output);
+
+
+        }
+
+        static void SumAndAverage(double v1, double v2)
+        {
+            double sum = 0;
+            double numCount = 0;
+            double average;
+            for (double i = v1; i <= v2; i++)
+            {
+                sum += i;
+                numCount++;
+            }
+            average = sum / numCount;
+            Console.WriteLine("Sum: " + sum + ", Average: " + average);
+        }
+
+        static void DrawTriangle()
+        {
+
+            int column = 1;
+            int row = 10;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int x = 0; x <= row - i; x++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write("*");
+                }
+                column += 2;
+                Console.WriteLine();
+
+
+            }
+        }
+
+        static void ToThePowerOf(int num, int pow)
+        {
+            int output = num;
+            for (int i = 0; i < pow - 1; i++)
+            {
+                output *= num;
+            }
+            Console.WriteLine(output);
+        }
+
     }
 }
